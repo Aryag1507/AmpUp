39532989427717ac8e57bc23e9925a4fd67412e4
boxkr <keeg9087@gmail.com>
2024-02-12 09:47:03 -0600

    init

---
+++ AmpUp/AmpUpApp.swift
@@ -0,0 +1,32 @@
+//
+//  AmpUpApp.swift
+//  AmpUp
+//
+//  Created by Keegan Reynolds on 2/12/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    print("Firebase Configured!")
+    return true
+  }
+}
+
+@main
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
---
+++ AmpUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AmpUp
+//
+//  Created by Keegan Reynolds on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
