a82467ed62a31391ef428092acf8d48626657a67
texjack19 <texjack19@gmail.com>
2024-02-14 17:08:43 -0600

    basic profile/edit UI

---
+++ AmpUp/EditProfilePic.swift
@@ -0,0 +1,22 @@
+//
+//  EditProfilePic.swift
+//  AmpUp
+//
+//  Created by Jack Sanchez on 2/14/24.
+//
+
+import SwiftUI
+
+struct EditProfilePic: View {
+
+    var body: some View {
+
+        Text("Edit Profile Picture")
+    }
+}
+
+struct EditPFP_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfilePic()
+    }
+}
